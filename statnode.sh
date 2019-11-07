c93e475035b35645c71b3771024196fccc26b275
Юрий <makarich@MacBook-Pro-Urij.local>
2019-11-07 10:17:09 +0300

    Fix

--- statnode.sh
+++ statnode.sh
@@ -2,7 +2,12 @@
 
 
 
+#stat() {
+
+
 printMetric() {
+#    echo "# HELP $1 $2"
+#    echo "# TYPE $1 $3"
     echo "$1 $4"
 }
 
@@ -11,13 +16,14 @@ api() { curl -s 127.0.0.1:14002/api/$1; }
 DASHDATA=`api "dashboard"`
 
 
-printMetric "node_id{node_id=\"`echo $DASHDATA | jq -r .data.nodeID`\", version=\"`echo $DASHDATA | jq -r '(.data.version.major|tostring) + "." + (.data.version.minor|tostring) + "." + (.data.version.patch|tostring)'`\"}" "node_id" "gauge" 1 
+printMetric "node_id{node_id=\"`echo $DASHDATA | jq -r .data.nodeID`\", version=\"`echo $DASHDATA | jq -r '(.data.version)'`\"}" "node_id" "gauge" 1 
 printMetric "diskspace_avail" "bandwidth_avail" "gauge" `echo $DASHDATA | jq -r '.data.diskSpace.available'`
 printMetric "diskspace_used" "bandwidth_avail" "gauge" `echo $DASHDATA | jq -r '.data.diskSpace.used'`
 printMetric "bandwidth_avail" "bandwidth_avail" "gauge" `echo $DASHDATA | jq -r '.data.bandwidth.available'`
 printMetric "bandwidth_used" "bandwidth_used" "gauge" `echo $DASHDATA | jq -r '.data.bandwidth.used'`
 printMetric "last_ping{}" "last_pinged" "gauge" `echo $DASHDATA | jq '.data.lastPinged | sub(".[0-9]+Z$"; "Z") | fromdate'`
 
+#echo $DASHDATA | jq -r '.data.satellites[].id'
 
 for sat in `echo $DASHDATA | jq -r '.data.satellites[].id'`
 do
